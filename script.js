828b1539ca1c4ad0aee77f3a2b6433495e2c316a
Benjamin Gambling <benjamin.gambling@gmail.com>
2020-12-15 17:23:13 -0800

    init

--- script.js
+++ script.js
@@ -1,50 +1,18 @@
 // NODE
-
-const csv = require("csv-parser");
 const fs = require("fs");
 
-const outputNode = [];
-
-fs.createReadStream("sample.csv")
-  .pipe(csv())
-  .on("data", (row) => {
-    let fullName = row.name.split(" ");
-    let person = {
-      first_name: fullName[0],
-      last_name: fullName[fullName.length - 1],
-      date_of_birth: row[" date of birth"],
-      address: row[" address"],
-      city: row[" city"],
-      country: row[" country"],
-    };
-
-    Object.keys(person).map(
-      (val) =>
-        (person[val] =
-          typeof person[val] === "string" ? person[val].trim() : person[val])
-    );
-
-    outputNode.push(person);
-  })
-  .on("end", () => console.log(outputNode));
-
-//   JAVASCRIPT
-const sample = `name, date of birth, address, city, country
-John Smith, 1989-01-01, 123 Fort Street, Victoria, Canada
-Amy C Adams, 1980-04-23, 464 Cook Street, Victoria, Canada
-Evan R, 1973-06-24, 11 Moore Drive, San Francisco, United States`;
+fs.readFile("sample.csv", { encoding: "utf-8" }, (err, data) => {
+  !err ? console.log(splitNames(data)) : console.log(err);
+});
 
 const splitNames = (samp) => {
   const output = [];
-  const arr = samp.split("\n");
-  //   REMOVE FIRST LINE OF VALUES
-  arr.shift();
 
+  const arr = samp.split("\n").filter(Boolean); // REMOVE ANY EMPTY STRINGS FROM PARSE
+  arr.shift(); //   REMOVE FIRST LINE OF VALUES
   arr.map((a) => {
-    //   SPLIT BY COMMA THEN TRIM EACH ITEM IN ARRAY
-    let arr = a.split(",").map((s) => s.trim());
-    // SPLIT NAME [0] VALUE TO GET BOTH FIRST AND LAST NAME
-    let fullName = arr[0].split(" ");
+    let arr = a.split(",").map((s) => s.trim()); //   SPLIT BY COMMA THEN TRIM EACH ITEM IN ARRAY
+    let fullName = arr[0].split(" "); // SPLIT NAME [0] VALUE TO GET BOTH FIRST AND LAST NAME
     let person = {
       first_name: fullName[0],
       last_name: fullName[fullName.length - 1], // WORKING BACKWARDS AVOIDS MIDDLE NAME
@@ -58,8 +26,6 @@ const splitNames = (samp) => {
   return output;
 };
 
-console.log(splitNames(sample));
-
 // BONUS
 const anagram = (w1, w2) => {
   if (w1.length != w2.length) return false;
@@ -68,4 +34,36 @@ const anagram = (w1, w2) => {
     : false;
 };
 
-console.log(anagram("mum", "mmu"));
+console.log(anagram("parallel of latitude", "parallel of altitude"));
+console.log(
+  anagram(
+    "supercalifragilisticexpialidocious",
+    "Hippopotomonstrosesquippedaliophob"
+  )
+);
+
+// USING CSV PARSE
+// const csv = require("csv-parser");
+
+// fs.createReadStream("sample.csv")
+//   .pipe(csv())
+//   .on("data", (row) => {
+//     let fullName = row.name.split(" ");
+//     let person = {
+//       first_name: fullName[0],
+//       last_name: fullName[fullName.length - 1],
+//       date_of_birth: row[" date of birth"],
+//       address: row[" address"],
+//       city: row[" city"],
+//       country: row[" country"],
+//     };
+
+//     Object.keys(person).map(
+//       (val) =>
+//         (person[val] =
+//           typeof person[val] === "string" ? person[val].trim() : person[val])
+//     );
+
+//     outputNode.push(person);
+//   })
+//   .on("end", () => console.log(outputNode));
